5b7c7ab38102d24247f408f7f1d399d913fd1aeb
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2025-05-23 15:49:22 -0300

    terminamos css y todo (tipo detalles)

--- TP04/Models/Ahorcado.cs
+++ TP04/Models/Ahorcado.cs
@@ -12,7 +12,11 @@ namespace TP04.Models
         public static int intentos { get; private set; }
         public static void InicializarPartida()
         {
-            palabras = new List<string>{"caminar", "botella", "maestro", "reunion", "apuntes", "cosecha"};
+            palabras = new List<string>{"caminar", "botella", "maestro", "reunion", "apuntes", "cosecha", "enlacez", "pintura", "alegría", "hogares",
+"montaña", "clasear", "esquema", "lectura", "visitas", "cuartos", "esferas", "calamar", "rescate", "bailaba",
+"nervios", "verdura", "tortuga", "aviones", "charlar", "pensado", "guitarra", "cuidaba", "amistad", "viajero",
+"cultura", "alcance", "retozar", "esperar", "cálculo", "rompido", "aliento", "soldado", "cuento", "cartón",
+"volaban", "mirador", "dejaban", "reflejo", "crianza", "afectar", "rosario", "soltera", "mandato", "ayudaba"};
             Random rnd = new Random();
             palabraElegida = palabras[rnd.Next(0, palabras.Count)];
             letrasUsadas = new List<char>();
